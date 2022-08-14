4c26841faf801b8129dab1668456fe3bef62156a
Dayana Castillo Lamas <lamasdayana@gmail.com>
2022-08-14 20:03:08 -0300

    Actividad de Python n°3

---
+++ Valoracion_Python.py
@@ -0,0 +1,18 @@
+# Cada estudiante deberá resolver UN (1) ejercicio a elección de los 10
+# ejercicios propuestos por el docente y subir en su propia rama del GitHub.
+
+# 3. Crear un lista en Python denominado “Perro” que contenga los
+# siguientes valores:
+# 13, Puppy, 12/12/2012 , Macho, 123
+# Que se corresponde con los datos de un perro de nuestra base de datos
+# (Id_Perro, nombre, fecha de nacimiento, sexo y dni del dueño). Modificar la
+# fecha de nacimiento por 13/12/2012 y reemplazar “dni del dueño” por 28957346
+
+perro = [13, "Puppy", "12/12/2012", "Macho", 123];
+
+perro[2] = "13/12/2012";
+
+perro[4] = 28957346;
+
+print(perro);
+
