060cd73174a592b532349b44bbb2b6340a82218f
Alvarez Jesica <alvarezjs1990@gmail.com>
2022-08-14 21:22:44 -0300

    Ejercicio N°12 Python

---
+++ Ejercicio_N_12.py
@@ -0,0 +1,15 @@
+'''
+Crear una tupla en Python con el nombre de “Historial4” la cual contenga los siguientes valores:
+7510, 7960, 76180, 800, 4100
+Que representa montos de atención en pesos por diferentes servicios/consultas de la mascota “Olivia”. Cree una función para determinar el valor mínimo de atención gastada en “Olivia” mostrándolo en pantalla.
+'''
+Historial4=(7510, 7960, 76180, 800, 4100)
+minimo= min (Historial4)
+
+print ("El valor mínimo de atención gastada en “Olivia” es: $",minimo)
+    
+
+
+
+
+
